c46b6a66003d5eb143a95054e01efc7af59a0fdc
joaofaggion17 <joaofaggion17@gmail.com>
2022-06-06 17:30:45 +0000

    Salvando

---
+++ Exercicio1.java
@@ -0,0 +1,17 @@
+public class Exercicio1 {
+
+    public static void main (String [] args){
+
+        int SegundaSemana = 5;
+        int TerceiraSemana = 2;
+        int QuartaSemana = 7;
+
+        System.out.println(SegundaSemana);
+        System.out.println(TerceiraSemana);
+        System.out.println(QuartaSemana);
+        
+
+    }
+    
+    
+}
